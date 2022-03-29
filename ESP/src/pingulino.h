10ebefe8825f2ca9c0a4d58e5446786874ae770e
unknown <rodrigo_daltoe@hotmail.com>
2022-03-28 21:50:23 -0300

    deleted obsolete library

--- ESP/src/pingulino.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "driver/gpio.h"
-#include "driver/timer.h"
-#include <esp_intr_alloc.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "sdkconfig.h"
